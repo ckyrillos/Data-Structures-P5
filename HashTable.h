736e87a5362ad7995d37f91b38c05e010c42292a
Carl <carl@thecorp.org>
2017-12-02 17:55:13 -0500

    First Commit

---
+++ HashTable.h
@@ -0,0 +1,23 @@
+/*
+ * HashTable.h
+ *
+ * COSC 160 Fall 2017
+ * Project #5
+ *
+ * Due on: DEC 8, 2017
+ * Author: Carl Kyrillos
+ *
+ *
+ * In accordance with the class policies and Georgetown's
+ * Honor Code, I certify that, with the exception of the
+ * class resources and those items noted below, I have neither
+ * given nor received any assistance on this project.
+ *
+ * References not otherwise commented within the program source code.
+ * Note that you should not mention any help from the TAs, the professor,
+ * or any code taken from the class textbooks.
+ */
+#ifndef PROJECT_5_HASHTABLE_H
+#define PROJECT_5_HASHTABLE_H
+
+#endif //PROJECT_5_HASHTABLE_H
---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Ethics Statement:
+ * Other than anything noted in the disclosures below, the code submitted is of my design and my design alone.
+ * I recieved no help nor used any references, resources, and had no discussion with other classmates.
+ * I understand the University policy concerning ethics and
+ * understand that violating this policy will result in drastic repercussions.
+ *
+ * Disclosures:
+ * Referred to Introduction to Algorithms 3rd Edition by Cormen, Leiserson, Rivest, and Stein
+ * Referred to Professor Bolton's Lecture Slides
+ *
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include "HashTable.h"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
